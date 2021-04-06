fc8b884ddc4a171b457379e1236cdf0b03753017
Josh <madrigaljosh10@yahoo.com>
2021-04-06 18:46:31 -0400

    Moving on figure it out later

--- Day46-IntermediateCreateSpotPlaylist/TimeMachine.py
+++ Day46-IntermediateCreateSpotPlaylist/TimeMachine.py
@@ -1,32 +1,45 @@
-date = input("Which year do you want to travel to? YYYY-MM-DD:")
-
 from bs4 import BeautifulSoup
 import requests
 import spotipy
 import config
 from spotipy.oauth2 import SpotifyOAuth
 
+# Scraping Billboard 100
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.find_all("span", class_="chart-element__information__song")
+song_names = [song.getText() for song in song_names_spans]
+
+#Spotify Authentication
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
         redirect_uri="http://example.com",
         client_id= config.clientId,
-        client_secret= config.clientSecret,
+        client_secret=config.clientSecret,
         show_dialog=True,
         cache_path="token.txt"
     )
 )
 user_id = sp.current_user()["id"]
+print(user_id)
 
-URL = "https://www.billboard.com/charts/hot-100/"
+#Searching Spotify for songs by title
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
 
-response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
-website_html = response.text
+#Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
 
-soup = BeautifulSoup(website_html, "html.parser")
-print(soup.title)
-song_span = soup.find_all("span", class_="chart-element__information__song")
-
-song_names = [song.getText() for song in song_span]
-
-print(song_names)
+#Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
--- Day46-IntermediateCreateSpotPlaylist/config.py
+++
@@ -1,2 +0,0 @@
-clientId = "f7fccbef836646bebc5af8bb86d81d8f"
-clientSecret = "3785070dc4844dc8823ebe0787ccf149"
